7be221697963a47b5c8407058efae7ad583f2ff5
Mike Spade <mikespadeny@gmail.com>
2022-12-04 02:00:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const messages = [
+  "Never",
+  "Always",
+  "Maybe",
+  "Sometimes",
+  "We shall see",
+  "Gonna need more info",
+  "Looks likely",
+  "Who knows?",
+  "Heck yeah",
+  "Hell Na",
+  "Stop it!",
+  "Quit Shaking Me!",
+  "Fa Sho"
+];
+
+
+const getRandomNumber = function() {
+  return Math.floor(Math.random() * messages.length);
+}
+
+/*
+    Go off and get a random number
+    then use that random number to pull a
+    random message from our array of possible responses
+*/
+const getRandomMessage = function() {
+  const randomNumber = getRandomNumber();
+  return messages[randomNumber];
+}
+
+/*
+  Using the getRandomMessage function above to input
+  a random message into the answer element
+*/
+const shakeyShakey = function() {
+  const answerElement = document.getElementById('answer');
+  answerElement.textContent = getRandomMessage();
+
+  const eightElement = document.getElementById('eight');
+  eightElement.textContent = '';
+
+  setTimeout(() => {
+    const eightElement = document.getElementById('eight');
+    eightElement.textContent = '8';
+
+    const answerElement = document.getElementById('answer');
+    answerElement.textContent = '';
+
+    const inputElement = document.getElementById('question');
+    inputElement.value = '';
+  }, 2000);
+};
+
+/*
+  Asking the browser to call shakeyShakey whenever the button is clicked
+*/
+const buttonElement = document.getElementById('button');
+buttonElement.addEventListener('click', shakeyShakey);
